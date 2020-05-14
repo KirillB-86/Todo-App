515ee89aa6a3efe05cbbaf4461c7ba2d4aae9fd1
Kirill Buturlin <cyrilbut@gmail.com>
2020-05-14 09:57:04 +0100

    v01

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("It is working!!!")
+var todoList = {
+  todos: ["item1", "item2", "item3"],
+
+  displayTodos() {
+    console.log("My Todos: ")
+    for (let i = 0; i < this.todos.length; i++) {
+      console.log(this.todos[i])
+    }
+  },
+
+  addTodo(item) {
+    this.todos.push(item)
+    this.displayTodos()
+  },
+
+  changeTodo(item, text) {
+    this.todos[item] = text
+    this.displayTodos()
+  },
+
+  deleteTodo(item) {
+    this.todos.splice(item, 1)
+    this.displayTodos()
+  },
+}
